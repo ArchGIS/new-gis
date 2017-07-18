f162c78a42af3b3788d9a1b05e2425507cdc78cb
Делюс Фархуллин <bontequero@gmail.com>
2017-07-18 17:53:28 +0300

    Added /monuments route

--- main.go
+++ main.go
@@ -1,26 +1,20 @@
 package main
 
 import (
-	"os"
-
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	"github.com/labstack/gommon/log"
 	"gopkg.in/jmcvetta/neoism.v1"
 
 	"github.com/ArchGIS/new-gis/assert"
 	middle "github.com/ArchGIS/new-gis/middlewares"
+	"github.com/ArchGIS/new-gis/routes"
 )
 
 var DB *neoism.Database
 
 func init() {
 	err := godotenv.Load()
-	assert.Nil(err)
-
-	neoHost := os.Getenv("Neo4jHost")
-	DB, err = neoism.Connect(neoHost + "db/data")
 	assert.Nil(err)
 }
 
@@ -32,10 +26,10 @@ func main() {
 	e.Use(middle.HandleOptions())
 	e.Use(middle.AddOrigin())
 	e.Use(middleware.Logger())
-	e.Logger.SetLevel(log.DEBUG)
 	e.Use(middleware.Recover())
 
 	e.POST("/login", loginHandler)
+	e.GET("/monuments", routes.Monuments)
 
 	e.Logger.Fatal(e.Start(":8181"))
 }
--- routes/monuments.go
+++ routes/monuments.go
@@ -1,1 +1,69 @@
 package routes
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/ArchGIS/ArchGoGIS/dbg"
+	"github.com/ArchGIS/new-gis/assert"
+	"github.com/jmcvetta/neoism"
+	"github.com/labstack/echo"
+)
+
+var DB *neoism.Database
+
+type monument struct {
+	// `json:` tags matches column names in query
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	ResName  string `json:"resName"`
+	Epoch    string `json:"epoch"`
+	SiteType string `json:"type"`
+}
+
+func Monuments(c echo.Context) error {
+	var err error
+	neoHost := os.Getenv("Neo4jHost")
+	DB, err = neoism.Connect(neoHost)
+	assert.Nil(err)
+	result, err := queryMonuments(c)
+
+	if err == nil {
+		return c.JSON(http.StatusOK, result)
+	}
+
+	return err
+}
+
+func queryMonuments(c echo.Context) ([]monument, error) {
+	var res []monument
+
+	cq := neoism.CypherQuery{
+		Statement: `
+			MATCH (s:Monument)<--(k:Knowledge)
+			WHERE k.monument_name =~ {name}
+			with s, k
+			MATCH (s)-[:has]->(st:MonumentType)
+			MATCH (s)-[:has]->(e:Epoch)
+			MATCH (r:Research)-[:has]->(k)
+			RETURN
+				s.id as id,
+				k.monument_name as name,
+				r.name as resName,
+				e.name as epoch,
+				st.name as type
+		`,
+		Parameters: neoism.Props{"name": "(?ui).*болг.*$"},
+		Result:     &res,
+	}
+
+	err := DB.Cypher(&cq)
+	assert.Nil(err)
+
+	dbg.Dump(res)
+	if len(res) > 0 {
+		return res, nil
+	}
+
+	return nil, echo.ErrNotFound
+}
