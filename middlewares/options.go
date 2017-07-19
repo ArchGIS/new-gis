7a9a8039d94e9428836947a4518484c541e7dea2
Делюс Фархуллин <bontequero@gmail.com>
2017-07-19 13:08:20 +0300

    fixed options headers handling

--- middlewares/options.go
+++ middlewares/options.go
@@ -17,7 +17,8 @@ func HandleOptions() echo.MiddlewareFunc {
 		return func(c echo.Context) error {
 			if c.Request().Method == "OPTIONS" {
 				c.Response().Header().Set("Allow", "OPTIONS, GET, POST")
-				c.Response().Header().Set("Access-Control-Allow-Headers", "Content-Type")
+				c.Response().Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+				return nil
 			}
 
 			return next(c)
