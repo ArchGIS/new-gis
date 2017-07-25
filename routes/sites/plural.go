fa083f087e5ecb4b3cd2ac707b89f0c75c2be63a
Делюс Фархуллин <bontequero@gmail.com>
2017-07-25 15:02:13 +0300

    added epoch and site type filters to /sites handler

--- routes/sites/plural.go
+++ routes/sites/plural.go
@@ -1,8 +1,10 @@
 package sites
 
 import (
+	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/ArchGIS/new-gis/assert"
 	"github.com/ArchGIS/new-gis/routes"
@@ -25,6 +27,8 @@ type (
 
 	requestParams struct {
 		Name   string `query:"site_name"`
+		Epoch  int    `query:"epoch_id"`
+		Type   int    `query:"type_id"`
 		Offset int    `query:"offset"`
 		Limit  int    `query:"limit"`
 	}
@@ -35,11 +39,10 @@ type (
 const (
 	statement = `
 		MATCH (s:Monument)<--(k:Knowledge)
-		WHERE k.monument_name =~ {name}
-		WITH s, k
 		MATCH (s)-[:has]->(st:MonumentType)
 		MATCH (s)-[:has]->(e:Epoch)
 		MATCH (r:Research)-[:has]->(k)
+		WHERE %s
 		RETURN
 			s.id AS id,
 			{
@@ -71,15 +74,17 @@ func querySites(c echo.Context) ([]site, error) {
 
 	var res []site
 
-	req := new(requestParams)
+	req := newRequestParams()
 	if err = c.Bind(req); err != nil {
 		return nil, routes.NotAllowedQueryParams
 	}
 
 	cq := neoism.CypherQuery{
-		Statement: statement,
+		Statement: finalStatement(statement, siteFilterString(req)),
 		Parameters: neoism.Props{
 			"name":   "(?ui).*" + req.Name + ".*$",
+			"epoch":  req.Epoch,
+			"type":   req.Type,
 			"offset": req.Offset,
 			"limit":  req.Limit,
 		},
@@ -94,4 +99,34 @@ func querySites(c echo.Context) ([]site, error) {
 	}
 
 	return []site{}, nil
+}
+
+func siteFilterString(reqParams *requestParams) string {
+	var filter []string
+
+	if reqParams.Name != "" {
+		filter = append(filter, "k.monument_name =~ {name}")
+	}
+	if reqParams.Epoch != 0 {
+		filter = append(filter, "e.id = {epoch}")
+	}
+	if reqParams.Type != 0 {
+		filter = append(filter, "st.id = {type}")
+	}
+
+	return strings.Join(filter, " AND ")
+}
+
+func finalStatement(statement, filter string) string {
+	return fmt.Sprintf(statement, filter)
+}
+
+func newRequestParams() *requestParams {
+	return &requestParams{
+		Name:   "",
+		Epoch:  0,
+		Type:   0,
+		Offset: 0,
+		Limit:  20,
+	}
 }
