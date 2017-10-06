a29b6eaaa289e69c6e11119332bcc81eee0d538a
Делюс Фархуллин <bontequero@gmail.com>
2017-10-06 17:24:43 +0300

    change cities struct to unexported

--- neo/epochs.go
+++ neo/epochs.go
@@ -4,12 +4,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type epoch struct {
+type epochProps struct {
 	ID   int64  `json:"id"`
 	Name string `json:"name"`
 }
 
-func (db *DB) Epochs(req gin.H) ([]epoch, error) {
+func (db *DB) Epochs(req gin.H) ([]epochProps, error) {
 	rows, err := db.QueryNeo(
 		epochsStatement,
 		gin.H{"language": req["lang"]},
@@ -24,9 +24,9 @@ func (db *DB) Epochs(req gin.H) ([]epoch, error) {
 		return nil, err
 	}
 
-	epochs := make([]epoch, len(data))
+	epochs := make([]epochProps, len(data))
 	for i, row := range data {
-		epochs[i] = epoch{
+		epochs[i] = epochProps{
 			ID:   row[0].(int64),
 			Name: row[1].(string),
 		}
